4cd481615236c541d66a70b709164ef1473034bf
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-07-11 16:49:12 +0200

    Initial commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,26 @@
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyHeader from "./components/MyHeader";
+import ArticleList from "./components/ArticlesList";
+import ArticlesDetails from "./components/ArticlesDetails";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <MyHeader />
+        <Routes>
+          <Route path="/" element={<ArticleList />} />
+          <Route path="/details/:article_id" element={<ArticlesDetails />} />
+
+          {/* <ArticleList/> */}
+          {/* <ArticlesDetails/> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
