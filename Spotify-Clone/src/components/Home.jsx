e33ffe7e94e54f3b0654bb1c79c64df82181b5e4
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-07-12 16:50:24 +0200

    commit 2

--- Spotify-Clone/src/components/Home.jsx
+++ Spotify-Clone/src/components/Home.jsx
@@ -14,3 +14,7 @@ function Homepage() {
 }
 
 export default Homepage;
+
+
+// ho avuto problemi con git.. mi ha praticamente sovrascritto questo progetto con il progetto di Spaceflight, per questo troverai un solo commit. Ho sclerato un pm a capire come committare, mi sono arresa alla sovrascrittura
+// per non parlare di una serie di problemi con le installazioni, tipo le icone di bootstrap, il solito dev che non mi da con il comando npm run dev..
